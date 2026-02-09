ff831d0635294b9178299cc2344e2861a2e9859b
43xingxing <17990861@qq.com>
2026-02-09 14:58:33 +0800

    Add dimension panel component

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app" class="app-container">
+    <DimensionPanel />
+  </div>
+</template>
+
+<script>
+import DimensionPanel from './components/DimensionPanel.vue'
+
+export default {
+  name: 'App',
+  components: {
+    DimensionPanel
+  }
+}
+</script>
+
+<style>
+html,
+body,
+#app {
+  height: 100%;
+  margin: 0;
+}
+
+.app-container {
+  height: 100%;
+  background: #f5f7fa;
+  padding: 16px;
+  box-sizing: border-box;
+}
+</style>
